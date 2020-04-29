a42a82f86b6861a19cbf82380e6535f96219dbeb
zhaolong <928343994@qq.com>
2020-04-29 20:46:21 +0800

    文本显示组件 Text

--- lib/code3/main_data62.dart
+++ lib/code3/main_data62.dart
@@ -2,8 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-///4.1
-///AppBar的基本使用
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
@@ -15,10 +14,8 @@ main() =>
         MaterialApp(
           home: FirstPage(),));
 
-///4.9 /lib/code3/main_data60.dart
-///FlexibleSpaceBar使用分析
-///SliverAppBar 配置可折叠的布局
-///经内内容布局效果
+///4.11 /lib/code3/main_data62.dart
+/// AppBar的shape属性分析
 class FirstPage extends StatefulWidget {
   @override
   _FirstPageState createState() => _FirstPageState();
@@ -33,22 +30,22 @@ class _FirstPageState extends State<FirstPage>{
     return buildBodyFunction();
   }
 
-
+ ///4.11 /lib/code3/main_data62.dart
+  /// AppBar的shape属性分析
   Widget buildBodyFunction() {
     return Scaffold(
       appBar: AppBar(
-        actions: [],
         title: Text("这是一个标题"),
         leading: Icon(Icons.home),
-        centerTitle: true,
-        brightness: Brightness.light,
-        textTheme: TextTheme(),
-        iconTheme: IconThemeData(),
-        actionsIconTheme: IconThemeData(),
-        primary: true,
-        shape:RoundedRectangleBorder(side: BorderSide(),borderRadius: BorderRadius.all(Radius.circular(10))) ,
-        elevation: 12,
-
+        shape:RoundedRectangleBorder(
+            //设置边框
+            side: BorderSide(
+              //边框的颜色
+                color: Colors.red,
+                //的宽度
+                width: 4),
+            //边框的圆角
+            borderRadius: BorderRadius.all(Radius.circular(40))) ,
       ),
       body: Center(child: Text("内容主体"),),);
   }
